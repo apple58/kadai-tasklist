721c26aa1e71cb5c53d628c356fa98a789ef1d31
asamin <applevr58@gmail.com>
2018-04-30 07:16:58 +0000

    Message-board modification

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,13 +9,8 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-
+*/
 
 Route::get('/', 'MessagesController@index');
 
 Route::resource('messages', 'MessagesController');
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
